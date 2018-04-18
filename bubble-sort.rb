4c6b5eb9feb1d61ed7835f4902a2e1ee1a8b25ad
maikimo <mike.p.monahan@gmail.com>
2018-04-17 22:41:56 -0400

    bubble sort

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,2 +1,19 @@
-def bub_sort array
-    
+def bub_sort(array)
+    loop do
+        swapped = false
+        (array.size - 1).times do |i|
+            if array[i] > array[i + 1]
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = true
+            end
+        end
+
+        break if not swapped
+    end
+
+    p array
+end
+
+random_array = Random.new_seed.to_s.split('')
+
+bub_sort(random_array)
